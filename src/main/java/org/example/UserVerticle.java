246aa980b41dc717a3424eda07bce0fd1a1d8dad
nikhilprakash <nikhilprakash510@gmail.com>
2025-11-20 00:56:24 +0530

    added user verticle methods

--- src/main/java/org/example/UserVerticle.java
+++ src/main/java/org/example/UserVerticle.java
@@ -4,7 +4,9 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonObject;
 
+import java.time.LocalTime;
 import java.util.UUID;
 
 public class UserVerticle extends AbstractVerticle {
@@ -13,18 +15,27 @@ public class UserVerticle extends AbstractVerticle {
     EventBus eventBus = null;
 
     @Override
-    public void start(Promise<Void> startPromise) throws Exception{
+    public void start(Promise<Void> startPromise) throws Exception {
         startPromise
                 .future()
                 .compose(this::setUserId)
-                .compose(this::setEventBus);
+                .compose(this::setEventBus)
+                .compose(this::getMsg)
+                .compose(this::sendMsg)
+                .onComplete(handle -> {
+                    if (handle.succeeded()) {
+                        System.out.println("User Verticle started for user: " + uuid);
+                    } else {
+                        System.out.println("User Verticle failed to start for user: " + uuid);
+                    }
+                });
     }
 
     // we need event bus to communicate with server verticle
     public Future<EventBus> setEventBus(Void v) {
         eventBus = vertx.eventBus();
         System.out.println("EventBus initialized for user: " + uuid);
-        return Future.future(p-> p.complete(eventBus));
+        return Future.future(p -> p.complete(eventBus));
     }
 
     // assign unique user ID
@@ -35,12 +46,20 @@ public class UserVerticle extends AbstractVerticle {
     }
 
     // receive message from server
-    public void getMsg(EventBus eb) {
-
+    public Future<EventBus> getMsg(EventBus eb) {
+        eb.consumer("USER_RECEIVE", handle -> {
+            System.out.println("@" + uuid + " received message: " + handle.body().toString());
+        });
+        return Future.future(p -> p.complete(eb));
     }
 
     // send message to server
-    public void sendMsg(EventBus eb) {
-
+    public Future<Void> sendMsg(EventBus eb) {
+        String msg = "New Message ".concat(LocalTime.now().toString());
+        JsonObject jo = new JsonObject();
+        jo.put("Sender", uuid);
+        jo.put("Message", msg);
+        eb.send("SERVER", jo);
+        return Future.future(Promise::complete);
     }
 }
